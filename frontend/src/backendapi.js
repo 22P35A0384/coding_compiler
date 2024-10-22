cb8d87f3b642934699ee08bba2d5941a80897b14
22P35A0384 <sai.gangadhar2022@outlook.com>
2024-10-22 16:13:30 +0530

    api

--- frontend/src/backendapi.js
+++ frontend/src/backendapi.js
@@ -1,8 +1,8 @@
 // const api = "https://api-7wntno63bq-uc.a.run.app";
 
-const api = "http://localhost:5000";
+// const api = "http://localhost:5000";
 
-// const api = "http://13.52.34.172:5000";
+const api = "http://13.52.34.172:5000";
 
 
 export default api;
